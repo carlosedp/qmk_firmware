a938e91a7faaf9a425029665de33731d770511bf
Carlos Eduardo <carlosedp@gmail.com>
2019-02-21 23:25:39 -0300

    Use grave/escape key combo
    https://docs.qmk.fm/#/feature_grave_esc

--- keyboards/xd60/keymaps/carlosedp/keymap.c
+++ keyboards/xd60/keymaps/carlosedp/keymap.c
@@ -5,7 +5,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // 0: Base Layer
   LAYOUT_all(
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_NO,      \
+      KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_NO,      \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSLS,    \
       MO(1),   KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,             KC_ENT,     \
       KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_GRAVE, KC_UP,    KC_RSFT,   \
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // 1: Function Layer
   LAYOUT_all(
-      KC_GRAVE,KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,               KC_F10,  KC_F11,    KC_F12,    KC_F13,   KC_NO,   \
+      KC_GESC,KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,               KC_F10,  KC_F11,    KC_F12,    KC_F13,   KC_NO,   \
       KC_NO,   BL_DEC,  BL_INC,  BL_ON,   BL_OFF,  BL_STEP, KC_NO,   KC_NO,   KC_NO,   KC_NO,               KC_MPLY, KC_BRID,   KC_BRIU,   KC_DEL,            \
       MO(1),   RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, RGB_MODE_PLAIN,      KC_NO,   KC_NO,     KC_NO,     KC_ENT,            \
       KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MUTE, KC_VOLD, KC_VOLU,    KC_NO,   KC_RSFT,   KC_PGUP,   KC_INS,            \
